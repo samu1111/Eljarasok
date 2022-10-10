8250476cb6b52b90d7489edacf5357b2325e0c8d
sami2004 <samuelkonderla@gmail.com>
2022-10-10 08:34:15 +0200

    Add project files.

---
+++ Eljarasok/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eljarasok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Uzenet("Kérek egy egész számot: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+
+            OsztokKiirasa(szam);
+
+            Uzenet("Egy másik egész számot: ");
+            szam = Convert.ToInt32(Console.ReadLine());
+
+            OsztokKiirasa(szam);
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+        static void Uzenet(string szoveg)
+        {
+            Console.Write($"{szoveg} ");
+        }
+
+        static void OsztokKiirasa(int number)
+        {
+            Console.WriteLine($"{number} osztói: ");
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
